df3dd9711a81fc0ff60d51a4b4044bdda2d09e45
Lucas Quaresma <lucas.quaresma1@gmail.com>
2025-12-07 16:39:19 -0300

    Day 1
    fix

---
+++ 2025/scala/day_1/day_1.scala
@@ -0,0 +1,89 @@
+import scala.io.Source
+import scala.util.{Try, Success, Failure}
+
+val inputFilePath: String = "./test_input"
+val startingPosition: Int = 50
+val validInputDirections: Set[String] = Set("L", "l", "R", "r")
+
+
+enum Direction:
+  case Left, Right
+
+object Direction {
+  def fromChar(c: Char): Direction = c match {
+    case 'L' | 'l' => Left
+    case 'R' | 'r' => Right
+    case _ => throw new IllegalArgumentException(s"Invalid direction: $c")
+  }
+}
+
+type Movement = (Direction, Int)
+
+def getFileMovements(src_path: String): List[Movement] = {
+  
+  def readFile(path: String): List[String] = {
+    val source = Source.fromFile(path)
+    return try {source.getLines().toList} finally {source.close()}
+  }
+
+  def parseMovement(s:String): Movement = {
+    require(
+      validInputDirections.contains(s(0).toString) && s.drop(1).toIntOption.isDefined,
+      s"Invalid input for the movement $s"
+    )
+    
+    return (Direction.fromChar(s(0)), s.drop(1).toInt)
+  }
+
+  return readFile(src_path).map(parseMovement)
+}
+
+
+def getClicks(movementList: List[Movement]): List[Int] = {
+  return movementList.map(
+    (dir, num) => dir match {
+      case Direction.Left => -1 * num
+      case Direction.Right => num
+    }
+  )
+}
+
+def getZeroPasses(positions: List[Int], clicks: List[Int]): List[Int] = {
+  positions.dropRight(1).zip(positions.tail).zip(clicks).map {
+    case ((startPos, endPos), click) =>
+      val n = startPos + click
+      val passes = (if (n >= 0) n else 99 - n) / 100
+
+      if (endPos == 0 && passes > 0) passes - 1 else passes
+  }
+} 
+
+
+def getPosition(fromPosition: Int, clicks: Int): Int = {
+  return (((fromPosition + clicks) % 100) + 100) % 100
+}
+
+
+@main
+def entry() = {
+  var instructions = getFileMovements(inputFilePath)
+  val clicks = getClicks(instructions)
+  
+  var positions = List(startingPosition)
+  for (c <- clicks) {
+    positions = positions :+ getPosition(positions.last, c)
+  }
+
+  val positionsOnZero = positions.map((p) => if (p == 0) then 1 else 0)
+  val passesOnZero = getZeroPasses(positions, clicks)
+
+  positions.dropRight(1).zip(positions.tail).zip(instructions).zip(passesOnZero).foreach {
+    case (((startPos, endPos), instruction), passes) =>
+      println(s"Position $startPos -> $endPos, \tinstruction $instruction, \tcrossed zero $passes time(s)")
+  }
+
+  println(s"\nTotal stops on position zero == ${positionsOnZero.sum}")
+  println(s"Total passes on position zero == ${passesOnZero.sum}")
+  println(s"Total passes + stops on position zero == ${positionsOnZero.sum + passesOnZero.sum}")
+}
+
---
+++ 2025/scala/shell.nix
@@ -0,0 +1,47 @@
+let
+  pconfig = { 
+    config = { 
+      allowUnfree = true;
+      cudaSupport = true;
+    };
+  };
+  pkgs =     import <nixpkgs>        pconfig;
+  unstable = import <nixos-unstable> pconfig;
+  slib =     pkgs.lib.strings;
+in
+
+pkgs.mkShell rec {
+  name = "AoC_2025_Scala";
+
+  pname = name;
+
+  buildInputs = with unstable; [
+    exercism
+    claude-code
+    gemini-cli
+    scala
+    scalafmt
+    scalafix
+  ];
+
+  shellHook = ''
+    export PATH="$JAVA_HOME/bin:$PATH"
+
+    function fok() { read -p "$1 (y/N): " && [[ $REPLY =~ ^([yY][eE][sS]|[yY])$ ]] }
+    
+    function code_here() {
+      nohup code --disable-gpu ./ >/dev/null 2>&1 && echo;      
+    }
+
+    function get_git_branch() {
+      git name-rev --name-only HEAD > /dev/null 2>&1
+      if [[ $? -eq 0 ]]; then
+        echo "($(git name-rev --name-only HEAD))";
+      else
+        echo "";
+      fi
+    };
+    PS1="\n\[\033[1;33m\][${name}:\w]\n\$(get_git_branch)\$\[\033[0m\] ";
+
+  '';
+}
