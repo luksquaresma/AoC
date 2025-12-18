c3814efe4d61b24e710c630272f8fdbf76bed2b1
Lucas Quaresma <lucas.quaresma@IFD-G7XN0JLQ4H.local>
2025-12-17 21:49:05 -0300

    Day 6
    fix

---
+++ 2025/scala/day_6/day_6.scala
@@ -0,0 +1,50 @@
+import scala.io.Source
+import scala.util.{Try, Success, Failure}
+
+val inputFilePath: String = "./day_6/input"
+
+type Operation = (String, List[Long])
+
+def getFileOperations(src_path: String): List[Operation] = {
+
+  def readFile(path: String): String = {
+    val source = Source.fromFile(path)
+    return try { source.mkString }
+    finally { source.close() }
+  }
+
+  def parseLine(s: String): List[String] = {
+    return s.trim.split(" ").filter(_.nonEmpty).map(_.trim()).toList
+  }
+
+  def parseOperation(ls: List[String]): Operation = {
+    require(
+      (ls.last == "+") | (ls.last == "*"),
+      s"Wrong operation type! \n  Invalid input for operation $ls!"
+    )
+
+    require(
+      ls.dropRight(1).forall(_.toLongOption.isDefined),
+      s"Wrong datatype for operation data! \n  Invalid input for operation $ls!"
+    )
+
+    return (ls.last, ls.dropRight(1).map(_.toLong))
+  }
+
+  return readFile(src_path).split("\n").map(parseLine(_)).toList.transpose.map(parseOperation(_))
+}
+
+def calculateOperation(op: Operation): Long = op(0) match {
+  case "+" => op(1).sum
+  case "*" => op(1).product
+  case _ => throw new IllegalArgumentException(s"Invalid operation type: ${op(0)}")
+}
+
+@main
+def entry() = {
+  var operations = getFileOperations(inputFilePath)
+
+  var results = operations.map(calculateOperation)
+
+  println(s"The sum of operations is ${results.sum}")
+}
--- 2025/scala/shell.nix
+++ 2025/scala/shell.nix
@@ -16,6 +16,7 @@ pkgs.mkShell rec {
   pname = name;
 
   buildInputs = with unstable; [
+    lazygit
     vscode
     exercism
     claude-code
